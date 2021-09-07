70df6b32e2b4eaca33df6d4f824f07ae2a0d8cbe
Deepak Nulu <deepak@celona.io>
2021-09-07 11:20:49 -0700

    Rename Model to MainModel in accordance with core

--- src/main/kotlin/org/treeWare/server/common/TreeWareServer.kt
+++ src/main/kotlin/org/treeWare/server/common/TreeWareServer.kt
@@ -3,9 +3,9 @@ package org.treeWare.server.common
 import org.apache.logging.log4j.LogManager
 import org.treeWare.metaModel.getMetaName
 import org.treeWare.metaModel.getRootMeta
-import org.treeWare.metaModel.newMetaMetaModel
+import org.treeWare.metaModel.newMainMetaMetaModel
 import org.treeWare.metaModel.validation.validate
-import org.treeWare.model.core.Model
+import org.treeWare.model.core.MainModel
 import org.treeWare.model.core.Resolved
 import org.treeWare.model.decoder.decodeJson
 import org.treeWare.model.encoder.encodeJson
@@ -21,12 +21,12 @@ class TreeWareServer(
     internal val rootName: String
 
     private val logger = LogManager.getLogger()
-    private val metaModel: Model<Resolved>
+    private val metaModel: MainModel<Resolved>
 
     // Validate the meta-model.
     init {
         logger.info("Meta-model file: $metaModelFilePath")
-        val metaMetaModel = newMetaMetaModel()
+        val metaMetaModel = newMainMetaMetaModel()
         val metaModelReader = ClassLoader.getSystemResourceAsStream(metaModelFilePath)?.let { InputStreamReader(it) }
             ?: throw IllegalStateException("Meta-model file not found")
         metaModel = decodeJson(metaModelReader, metaMetaModel, "data") { null }

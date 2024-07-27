e036ebcfe1e7fb007f0a37dbeff64310ef06df33
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2024-07-27 15:18:20 -0700

    Update library version to 0.2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // conveys this.
 allprojects {
     group = "org.tree-ware.tree-ware-kotlin-server"
-    version = "0.1.0.1"
+    version = "0.2.0.0"
 }
 
 val ktorApiKeyVersion = "1.1.0"
@@ -16,7 +16,7 @@ val mockkVersion = "1.12.0"
 plugins {
     kotlin("jvm") version "1.7.0"
     id("idea")
-    id("org.tree-ware.core") version "0.1.0.0-SNAPSHOT" // TODO #### drop -SNAPSHOT
+    id("org.tree-ware.core") version "0.1.0.0"
     id("java-library")
     id("maven-publish")
 }
@@ -24,7 +24,6 @@ plugins {
 repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
-    mavenLocal() // TODO #### delete
 }
 
 tasks.withType<KotlinCompile> {
